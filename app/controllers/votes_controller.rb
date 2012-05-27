1ae2354ba3b1067b977e8ac5ac42b4181f046d41
Aaron Parecki <aaron@parecki.com>
2012-05-27 15:09:50 -0700

    Update the user's vote if they vote more than once.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,56 +1,63 @@
 class VotesController < ApplicationController
   # GET /votes
   # GET /votes.json
-  def index
-    authenticate
-    @votes = Vote.all
+  # def index
+  #   authenticate
+  #   @votes = Vote.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @votes }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # index.html.erb
+  #     format.json { render json: @votes }
+  #   end
+  # end
 
   # GET /votes/1
   # GET /votes/1.json
-  def show
-    authenticate
-    @vote = Vote.find(params[:id])
+  # def show
+  #   authenticate
+  #   @vote = Vote.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @vote }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @vote }
+  #   end
+  # end
 
   # GET /votes/new
   # GET /votes/new.json
-  def new
-    authenticate
-    @vote = Vote.new
+  # def new
+  #   authenticate
+  #   @vote = Vote.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @vote }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @vote }
+  #   end
+  # end
 
-  # GET /votes/1/edit
-  def edit
-    authenticate
-    @vote = Vote.find(params[:id])
-  end
+  # # GET /votes/1/edit
+  # def edit
+  #   authenticate
+  #   @vote = Vote.find(params[:id])
+  # end
 
   # POST /votes
   # POST /votes.json
   def create
     authenticate
 
-    puts params.inspect
+    value = (params[:vote_value] == "yes" ? 1 : 0)
 
-    @vote = Vote.new(params[:vote])
-    @vote.value = (params[:vote_value] == "yes" ? 1 : 0)
-    @vote.shareholder = @current_user
+    # First check if this user has already voted
+    vote = Vote.where(:candidate_id => params[:vote][:candidate_id], :shareholder_id => @current_user.id)
+    if vote.exists?
+      @vote = vote[0]
+      @vote.value = value
+    else
+      @vote = Vote.new(params[:vote])
+      @vote.value = value
+      @vote.shareholder = @current_user
+    end
 
     respond_to do |format|
       if @vote.save
@@ -63,33 +70,33 @@ class VotesController < ApplicationController
     end
   end
 
-  # PUT /votes/1
-  # PUT /votes/1.json
-  def update
-    authenticate
-    @vote = Vote.find(params[:id])
+  # # PUT /votes/1
+  # # PUT /votes/1.json
+  # def update
+  #   authenticate
+  #   @vote = Vote.find(params[:id])
 
-    respond_to do |format|
-      if @vote.update_attributes(params[:vote])
-        format.html { redirect_to @vote, notice: 'Vote was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @vote.update_attributes(params[:vote])
+  #       format.html { redirect_to @vote, notice: 'Vote was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @vote.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /votes/1
   # DELETE /votes/1.json
-  def destroy
-    authenticate
-    @vote = Vote.find(params[:id])
-    @vote.destroy
+  # def destroy
+  #   authenticate
+  #   @vote = Vote.find(params[:id])
+  #   @vote.destroy
 
-    respond_to do |format|
-      format.html { redirect_to votes_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to votes_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
